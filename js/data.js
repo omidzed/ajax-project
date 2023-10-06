15cf25a713a153f270aa9ff11f5d24e5087b7a50
Omid Asadi <omiddzedd@gmail.com>
2023-10-06 23:49:25 +0000

    making initial commit, note to self, I worked out of order and wrote code in data.js even before creating a new branch for my issue 1 to work on, be prepared to deal with any issues this may cause.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,17 @@
 /* exported data */
+
+let data = {
+  view: 'home-page',
+  assets: [],
+  editing: null,
+  nextAssetId: 1
+};
+
+const previousEntriesJSON = localStorage.getItem('javascript-local-storage');
+if (previousEntriesJSON !== null) {
+  data = JSON.parse(previousEntriesJSON);
+}
+window.addEventListener('beforeunload', function (event) {
+  const entriesJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', entriesJSON);
+});
